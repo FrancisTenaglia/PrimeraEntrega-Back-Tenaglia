08b51ca9a2eea15b18dc0e1fd7a223a50702dc35
FrancisTenaglia <fgtenaglia@alumnos.fi.mdp.edu.ar>
2023-04-05 21:17:32 -0300

    add Src file y  AppJs pero sin completar

--- src/App.js
+++ src/App.js
@@ -11,16 +11,23 @@ server.get('/',(req, res) => {
     res.send('Servidor Express')
 });
 
+//Este endpoint tiene que devolver todos los productos en /products
 server.get('/products', (req, res) => {
-    res.send(` ${req.params.products}`)
+    res.send(` ${products}`)
 });
 
-server.get('/products/:limit', (req, res) => {
-    res.send(`Aca va a ir los productos con el limite`)
+//Este endpoint tiene uqe devolver en este caso /products?limit=5, los primeros 5 productos
+server.get('/productsLimit', (req, res) => {
+    res.send(` ${req.query.limit}`)
 });
 
+//Este endpoint tiene que devolver por ejemplo /products/2 el producto con el Id=2 y si no hay producto  con ese id, tiene que devolver un objeto con un error
 server.get('/products/:productId', (req, res) => {
     res.send(` ${req.params.productId}`)
 });
+
+
+
+
 
 
