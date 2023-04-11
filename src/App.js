ce39de51a340db66f7b0dcc63832669dc2ddc33c
FrancisTenaglia <fgtenaglia@alumnos.fi.mdp.edu.ar>
2023-04-11 20:18:30 -0300

    agregue try y catch en manager, y arreglo en App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-const express = require('express');
 
-const ProductManager = require('../src/components/ProductManager');
+const express = require('express');
+const {ProductManager} = require('./components/ProductManager');
 
 const puerto = 8080;
-
-const App = express;
+const server = express();
+server.use(express.json);
+server.use(express.urlencoded({extended: true}));
 
 //Aca inicializo el PM importado
-const inicializo = new ProductManager('./productos.json');
+const productManager = new ProductManager('./productos.json');
 
-//Este endpoint tiene uqe devolver en este caso si no tiene limit, va a mostrar 5, sino, deberia mostrar todos
-App.get('/productsLimit', (req, res) => {
-    res.json(inicializo.getProducts(req.params.limit || 5))
+//Este endpoint tiene uqe devolver en este caso si no tiene limit, pero con un limit =5 por ejemplo mostraria los primeros 5, sino todos
+server.get('/products', (req, res) => {
+    res.json(productManager.getProducts(req.query.limit ||0));
 });
 
 //Este endpoint tiene que devolver por ejemplo /products/2 el producto con el Id=2 y si no hay producto  con ese id, tiene que devolver un objeto con un error
-App.get('/products/:productId', (req, res) => {
-    res.json(inicializo.getProductsById(req.params.productId))
+server.get('/products/:productId', (req, res) => {
+    res.json(productManager.getProductsById(req.params.productId))
 });
 
-App.listen(8080, () => {
-    console.log('Corriendo en el puerto 8080');
-  });
+server.listen(puerto, () => {
+    console.log('Corriendo en el puerto ', puerto);
+});
 
 
 
